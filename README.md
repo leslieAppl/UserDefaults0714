e67ec55e5801b27e52ec602cd7ff99d978cb2a6a
Leslie <leslie.appl@gmail.com>
2020-07-14 13:50:44 +0800

    To keep track of the time the user has spent without using the app.

---
+++ README.md
@@ -0,0 +1,6 @@
+# UserDefaults
+
+## Storing values in the User Defaults system.
+
+## To keep track of the time the user has spent without using the app.
+
--- UserDefaults0714/ViewController.swift
+++ UserDefaults0714/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UIStepper!
+    @IBOutlet weak var counterLbl: UILabel!
+    @IBOutlet weak var reminderLbl: UILabel!
+    
+    var defaultValues: UserDefaults!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ///Get a reference to the UserDefaults object.
+        defaultValues = UserDefaults.standard
+        
+        if let number = defaultValues.object(forKey: "counter") as? Double {
+            counter.value = number
+            counterLbl.text = String(number)
+        }
+        
+        //TODO: To keep track of the time the user has spent without using the app.
+        if let lastDate = defaultValues.object(forKey: "lastDate") as? Date {
+            ///Get a reference to the user’s current calendar object
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: lastDate, to: Date())
+            let dateDifference = "\(components.year!) Years, \(components.month!) Months, \(components.day!) days, \(components.hour!) hours, \(components.minute!) minutes, \(components.second!) seconds"
+            reminderLbl.text = dateDifference
+            
+        }
+        defaultValues.set(Date(), forKey: "lastDate")
     }
 
-
+    @IBAction func incrementValue(_ sender: UIStepper) {
+        let current = sender.value
+        
+        defaultValues.set(current, forKey: "counter")
+        counterLbl.text = String(current)
+    }
+    
 }
 
